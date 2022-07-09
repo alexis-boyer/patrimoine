bffacd0f43d67179552ff38424150289b1a4d312
Alexis BOYER <alexis.boyer@gmail.com>
2022-07-09 19:35:26 +0200

    add buildings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Building } from './building';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'patrimoine';
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
 }
---
+++ src/app/building.ts
@@ -0,0 +1,10 @@
+export class Building {
+
+    constructor(
+      public id: number,
+      public name: string,
+      public address: string,
+      public description?: string
+    ) {  }
+  
+  }
